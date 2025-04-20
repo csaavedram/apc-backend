9a5e6d577112fe41ca25d98cb0e963cc4dac66ca
Gaston <gaston.rodriguez@icloud.com>
2025-04-20 02:25:19 -0500

    client removed, usuario updated

--- src/main/java/com/apcemedicom/repositorios/ClienteRepository.java
+++
@@ -1,5 +0,0 @@
-package com.apcemedicom.repositorios;
-import com.apcemedicom.modelo.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
