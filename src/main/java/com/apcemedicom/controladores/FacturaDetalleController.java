57bc72c3aba4964583073f82765701b6981b3baa
Christian Saavedra <chrissaavedra2002@outlook.es>
2025-06-20 13:34:28 -0500

    fix

--- src/main/java/com/apcemedicom/controladores/FacturaDetalleController.java
+++ src/main/java/com/apcemedicom/controladores/FacturaDetalleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,7 +69,7 @@ public class FacturaDetalleController {
   @GetMapping("/")
   public ResponseEntity<?> listarFacturas() {
     return ResponseEntity.ok(facturaDetalleService.obtenerDetallesFacturas());
-
+  }
   
   @GetMapping("/factura/{facturaId}/con-series")
   public ResponseEntity<List<FacturaDetalle>> listarFacturaDetailsPorFacturaConSeries(@PathVariable("facturaId") Factura facturaId) {
